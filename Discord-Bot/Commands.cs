00f2989235add0a070c1beddfa7738cbdbffba4a
Victor Fong <djtekno11@gmail.com>
2019-04-29 02:53:44 -0600

    First Stable Release with limited abilities. pongs when pinged.
    added music dependencies (for windows) to allow for future voice ability
    
    //TODO: interactivity

---
+++ Discord-Bot/Commands.cs
@@ -0,0 +1,29 @@
+/*
+ * Class: Commands
+ *
+ * Purpose: Holds and executes commands triggered by users.
+ */
+
+
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+
+namespace Discord_Bot
+{
+	public class UngroupedCommands
+	{
+		[Command("ping")] //define method
+		[System.ComponentModel.Description("pongs when pinged")] //describe command
+		[Aliases("pong")] //any other words for command
+		public async Task ping(CommandContext context) //no additional arguments are taken
+		{
+			await context.TriggerTypingAsync(); //show in discord as typing
+
+			var emoji = DiscordEmoji.FromName(context.Client, ":ping_pong:");	//creat emoji just cuz
+			await context.RespondAsync($"{emoji} pong! {context.Client.Ping}ms");	//respond
+		}
+	}
+}
