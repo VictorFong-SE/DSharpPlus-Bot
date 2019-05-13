22fde7a46813c1a015406ae2c74b266c986ed497
Victor Fong <vfong3@uic.edu>
2019-05-12 19:03:35 -0500

    Changes to CommandsAdmin

--- Discord-Bot/Client.cs
+++ Discord-Bot/Client.cs
@@ -22,197 +22,198 @@ using Newtonsoft.Json;
 
 namespace Discord_Bot
 {
-    public class Client
-    {
-        public DiscordClient          client        { get; set; } // discord client itself
-        public InteractivityExtension interactivity { get; set; } //module to handle interactible commands
-        public CommandsNextExtension  commands      { get; set; } // module to overwatch general commands
+   public class Client
+   {
+      public DiscordClient          client        { get; set; } // discord client itself
+      public InteractivityExtension interactivity { get; set; } //module to handle interactible commands
+      public CommandsNextExtension  commands      { get; set; } // module to overwatch general commands
 
 
-        public async Task MainAsync()
-        {
-            // init config
-            if (!File.Exists("config.json"))
-            {
-                #region !! config unavailable !!
+      public async Task MainAsync()
+      {
+         // init config
+         if (!File.Exists("config.json"))
+         {
+            #region !! config unavailable !!
 
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
 
-                centerWrite("▒▒▒▒▒▒▒▒▒▄▄▄▄▒▒▒▒▒▒▒", 2);
-                centerWrite("▒▒▒▒▒▒▄▀▀▓▓▓▀█▒▒▒▒▒▒");
-                centerWrite("▒▒▒▒▄▀▓▓▄██████▄▒▒▒▒");
-                centerWrite("▒▒▒▄█▄█▀░░▄░▄░█▀▒▒▒▒");
-                centerWrite("▒▒▄▀░██▄░░▀░▀░▀▄▒▒▒▒");
-                centerWrite("▒▒▀▄░░▀░▄█▄▄░░▄█▄▒▒▒");
-                centerWrite("▒▒▒▒▀█▄▄░░▀▀▀█▀▒▒▒▒▒");
-                centerWrite("▒▒▒▄▀▓▓▓▀██▀▀█▄▀▀▄▒▒");
-                centerWrite("▒▒█▓▓▄▀▀▀▄█▄▓▓▀█░█▒▒");
-                centerWrite("▒▒▀▄█░░░░░█▀▀▄▄▀█▒▒▒");
-                centerWrite("▒▒▒▄▀▀▄▄▄██▄▄█▀▓▓█▒▒");
-                centerWrite("▒▒█▀▓█████████▓▓▓█▒▒");
-                centerWrite("▒▒█▓▓██▀▀▀▒▒▒▀▄▄█▀▒▒");
-                centerWrite("▒▒▒▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
-                Console.BackgroundColor = ConsoleColor.Yellow;
-                centerWrite("WARNING", 3);
-                Console.ResetColor();
-                centerWrite("Thank you Mario!", 1);
-                centerWrite("But our config.json is in another castle!");
-                centerWrite("(Please place config.json within working directory with working values.)", 2);
-                centerWrite("Press any key to exit..",                                                  1);
-                Console.SetCursorPosition(0, 0);
-                Console.ReadKey();
+            centerWrite("▒▒▒▒▒▒▒▒▒▄▄▄▄▒▒▒▒▒▒▒", 2);
+            centerWrite("▒▒▒▒▒▒▄▀▀▓▓▓▀█▒▒▒▒▒▒");
+            centerWrite("▒▒▒▒▄▀▓▓▄██████▄▒▒▒▒");
+            centerWrite("▒▒▒▄█▄█▀░░▄░▄░█▀▒▒▒▒");
+            centerWrite("▒▒▄▀░██▄░░▀░▀░▀▄▒▒▒▒");
+            centerWrite("▒▒▀▄░░▀░▄█▄▄░░▄█▄▒▒▒");
+            centerWrite("▒▒▒▒▀█▄▄░░▀▀▀█▀▒▒▒▒▒");
+            centerWrite("▒▒▒▄▀▓▓▓▀██▀▀█▄▀▀▄▒▒");
+            centerWrite("▒▒█▓▓▄▀▀▀▄█▄▓▓▀█░█▒▒");
+            centerWrite("▒▒▀▄█░░░░░█▀▀▄▄▀█▒▒▒");
+            centerWrite("▒▒▒▄▀▀▄▄▄██▄▄█▀▓▓█▒▒");
+            centerWrite("▒▒█▀▓█████████▓▓▓█▒▒");
+            centerWrite("▒▒█▓▓██▀▀▀▒▒▒▀▄▄█▀▒▒");
+            centerWrite("▒▒▒▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            centerWrite("WARNING", 3);
+            Console.ResetColor();
+            centerWrite("Thank you Mario!", 1);
+            centerWrite("But our config.json is in another castle!");
+            centerWrite("(Please place config.json within working directory with working values.)", 2);
+            centerWrite("Press any key to exit..",                                                  1);
+            Console.SetCursorPosition(0, 0);
+            Console.ReadKey();
 
-                #endregion
+            #endregion
 
-                Environment.Exit(0);
-            }
+            Environment.Exit(0);
+         }
 
-            string json;
-            using (var fileLoc = File.OpenRead("config.json"))
-                using (var streamReader = new StreamReader(fileLoc, new UTF8Encoding(false)))
-                    json = await streamReader.ReadToEndAsync();
+         string json;
+         using (var fileLoc = File.OpenRead("config.json"))
+            using (var streamReader = new StreamReader(fileLoc, new UTF8Encoding(false)))
+               json = await streamReader.ReadToEndAsync();
 
-            // load values from json file
-            var jsonConfig = JsonConvert.DeserializeObject<JsonConfig>(json);
-            var config = new DiscordConfiguration
-                         {
-                             Token     = jsonConfig.Token, //set token
-                             TokenType = TokenType.Bot,
+         // load values from json file
+         var jsonConfig = JsonConvert.DeserializeObject<JsonConfig>(json);
+         var config = new DiscordConfiguration
+                      {
+                         Token     = jsonConfig.Token, //set token
+                         TokenType = TokenType.Bot,
 
-                             AutoReconnect         = true, // try to reconnect if connection lost
-                             UseInternalLogHandler = true,
-                             LogLevel              = LogLevel.Debug //show log in terminal
-                         };
+                         AutoReconnect         = true, // try to reconnect if connection lost
+                         UseInternalLogHandler = true,
+                         LogLevel              = LogLevel.Debug //show log in terminal
+                      };
 
-            client = new DiscordClient(config);
+         client = new DiscordClient(config);
 
-            //setup client debugging
-            client.Ready          += discordClientReady;
-            client.GuildAvailable += discordClientHasGuild;
-            client.ClientErrored  += discordClientError;
-            
-            //initialize interactivity
-            client.UseInteractivity(new InteractivityConfiguration
-                                    {
-                                        //by default, commands should ignore reactions
-                                        PaginationBehaviour = PaginationBehaviour.Ignore,
-                                        //default timeout
-                                        Timeout = TimeSpan.FromMinutes(2)
-                                    });
-            
-            //initialize commands
-            var commandConfig = new CommandsNextConfiguration
-                                {
-                                    StringPrefixes      = jsonConfig.CommandPrefixes,
-                                    EnableDms           = true,
-                                    EnableMentionPrefix = true
-                                };
-            commands = client.UseCommandsNext(commandConfig);
+         //setup client debugging
+         client.Ready          += discordClientReady;
+         client.GuildAvailable += discordClientHasGuild;
+         client.ClientErrored  += discordClientError;
 
-            //setup command debugging
-            commands.CommandExecuted += commandExecuted;
-            commands.CommandErrored  += commandError;
+         //initialize interactivity
+         client.UseInteractivity(new InteractivityConfiguration
+                                 {
+                                    //by default, commands should ignore reactions
+                                    PaginationBehaviour = PaginationBehaviour.Ignore,
+                                    //default timeout
+                                    Timeout = TimeSpan.FromMinutes(2)
+                                 });
 
-            //register commands
-            commands.RegisterCommands<Commands>();
-            commands.RegisterCommands<InteractiveCommands>();
+         //initialize commands
+         var commandConfig = new CommandsNextConfiguration
+                             {
+                                StringPrefixes      = jsonConfig.CommandPrefixes,
+                                EnableDms           = true,
+                                EnableMentionPrefix = true
+                             };
+         commands = client.UseCommandsNext(commandConfig);
 
+         //setup command debugging
+         commands.CommandExecuted += commandExecuted;
+         commands.CommandErrored  += commandError;
 
-            //choose help formatter (default built into api)
-            commands.SetHelpFormatter<DefaultHelpFormatter>();
+         //register commands
+         commands.RegisterCommands<CommandsUngrouped>();
+         commands.RegisterCommands<CommandsAdmin>();
+         commands.RegisterCommands<InteractiveCommands>();
 
 
-            await client.ConnectAsync(); //connect bot
-
-            //If doing something with bot here is the place to do it
-
-            await Task.Delay(-1); //prevent death
-        }
+         //choose help formatter (default built into api)
+         commands.SetHelpFormatter<DefaultHelpFormatter>();
 
 
-        /*#######---Helper Functions---#######*/
-        private Task discordClientReady(ReadyEventArgs events)
-        {
-            // let's log the fact that this event occured
-            events.Client.DebugLogger.LogMessage(LogLevel.Info, "Patto", "is ready to roll!", DateTime.Now);
+         await client.ConnectAsync(); //connect bot
 
-            //if a method is not async, a Task must be returned
-            return Task.CompletedTask;
-        }
+         //If doing something with bot here is the place to do it
 
-        private Task discordClientHasGuild(GuildCreateEventArgs events)
-        {
-            // log name of server requesting bot
-            events.Client.DebugLogger.LogMessage(LogLevel.Info, "Patto", $"is in server: {events.Guild.Name}",
-                                                 DateTime.Now);
-
-            //if a method is not async, a Task must be returned
-            return Task.CompletedTask;
-        }
-
-        private Task discordClientError(ClientErrorEventArgs events)
-        {
-            //log error details
-            events.Client.DebugLogger.LogMessage(LogLevel.Error, "Patto",
-                                                 $"Exception occured: {events.Exception.GetType()}: {events.Exception.Message}",
-                                                 DateTime.Now);
-
-            //if a method is not async, a Task must be returned
-            return Task.CompletedTask;
-        }
+         await Task.Delay(-1); //prevent death
+      }
 
 
-        private Task commandExecuted(CommandExecutionEventArgs events)
-        {
-            // log command details
-            events.Context.Client.DebugLogger.LogMessage(LogLevel.Info, "Patto",
-                                                         $"{events.Context.User.Username} executed command '{events.Command.QualifiedName}'",
-                                                         DateTime.Now);
+      /*#######---Helper Functions---#######*/
+      private Task discordClientReady(ReadyEventArgs events)
+      {
+         // let's log the fact that this event occured
+         events.Client.DebugLogger.LogMessage(LogLevel.Info, "Patto", "is ready to roll!", DateTime.Now);
 
-            //if a method is not async, a Task must be returned
-            return Task.CompletedTask;
-        }
+         //if a method is not async, a Task must be returned
+         return Task.CompletedTask;
+      }
 
-        private async Task commandError(CommandErrorEventArgs events)
-        {
-            // log error details
-            events.Context.Client.DebugLogger.LogMessage(LogLevel.Error, "Patto",
-                                                         $"{events.Context.User.Username} tried executing '{events.Command?.QualifiedName ?? "<unknown command>"}' but it errored: {events.Exception.GetType()}: {events.Exception.Message ?? "<no message>"}",
-                                                         DateTime.Now);
+      private Task discordClientHasGuild(GuildCreateEventArgs events)
+      {
+         // log name of server requesting bot
+         events.Client.DebugLogger.LogMessage(LogLevel.Info, "Patto", $"is in server: {events.Guild.Name}",
+                                              DateTime.Now);
 
-            // check for proper perms
-            if (events.Exception is ChecksFailedException)
-            {
-                var emoji = DiscordEmoji.FromName(events.Context.Client, ":no_entry:");
+         //if a method is not async, a Task must be returned
+         return Task.CompletedTask;
+      }
 
-                //wrap response into an embed
-                var embed = new DiscordEmbedBuilder
-                            {
-                                Title       = "Access denied",
-                                Description = $"{emoji} Maybe when you're older... (incorrect permissions)",
-                                Color       = new DiscordColor(0xFF0000) // choose color (red)
-                            };
-                await events.Context.RespondAsync("", embed: embed);
-            }
-        }
+      private Task discordClientError(ClientErrorEventArgs events)
+      {
+         //log error details
+         events.Client.DebugLogger.LogMessage(LogLevel.Error, "Patto",
+                                              $"Exception occured: {events.Exception.GetType()}: {events.Exception.Message}",
+                                              DateTime.Now);
+
+         //if a method is not async, a Task must be returned
+         return Task.CompletedTask;
+      }
 
 
-        private static void centerWrite(string buffer, int linesToSkip = 0) // writes to center of console
-        {
-            for (var i = 0; i < linesToSkip; i++)
-                Console.WriteLine();
+      private Task commandExecuted(CommandExecutionEventArgs events)
+      {
+         // log command details
+         events.Context.Client.DebugLogger.LogMessage(LogLevel.Info, "Patto",
+                                                      $"{events.Context.User.Username} executed command '{events.Command.QualifiedName}'",
+                                                      DateTime.Now);
 
-            Console.SetCursorPosition((Console.WindowWidth - buffer.Length) / 2, Console.CursorTop);
-            Console.WriteLine(buffer);
-        }
-    }
+         //if a method is not async, a Task must be returned
+         return Task.CompletedTask;
+      }
+
+      private async Task commandError(CommandErrorEventArgs events)
+      {
+         // log error details
+         events.Context.Client.DebugLogger.LogMessage(LogLevel.Error, "Patto",
+                                                      $"{events.Context.User.Username} tried executing '{events.Command?.QualifiedName ?? "<unknown command>"}' but it errored: {events.Exception.GetType()}: {events.Exception.Message ?? "<no message>"}",
+                                                      DateTime.Now);
+
+         // check for proper perms
+         if (events.Exception is ChecksFailedException)
+         {
+            var emoji = DiscordEmoji.FromName(events.Context.Client, ":no_entry:");
+
+            //wrap response into an embed
+            var embed = new DiscordEmbedBuilder
+                        {
+                           Title       = "Access denied",
+                           Description = $"{emoji} Maybe when you're older... (incorrect permissions)",
+                           Color       = new DiscordColor(0xFF0000) // choose color (red)
+                        };
+            await events.Context.RespondAsync("", embed: embed);
+         }
+      }
 
 
-    public struct JsonConfig // struct created to hold token
-    {
-        [JsonProperty("token")] public string Token { get; private set; }
+      private static void centerWrite(string buffer, int linesToSkip = 0) // writes to center of console
+      {
+         for (var i = 0; i < linesToSkip; i++)
+            Console.WriteLine();
 
-        [JsonProperty("commandPrefixes")] public string[] CommandPrefixes { get; private set; }
-    }
+         Console.SetCursorPosition((Console.WindowWidth - buffer.Length) / 2, Console.CursorTop);
+         Console.WriteLine(buffer);
+      }
+   }
+
+
+   public struct JsonConfig // struct created to hold token
+   {
+      [JsonProperty("token")] public string Token { get; private set; }
+
+      [JsonProperty("commandPrefixes")] public string[] CommandPrefixes { get; private set; }
+   }
 }
--- Discord-Bot/Commands.cs
+++ Discord-Bot/Commands.cs
@@ -9,61 +9,143 @@
 
 using System;
 using System.Threading.Tasks;
+using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.CommandsNext.Exceptions;
 using DSharpPlus.Entities;
-
 
 namespace Discord_Bot
 {
-	public class Commands : BaseCommandModule
-	{
-		//attributes
-		[Command("ping")] //define method
-		[Description("pongs when pinged.")] //describe command
-		[Aliases("pong", "ping-pong")] //any other words for command
-		public async Task ping(CommandContext context) //no additional arguments are taken
-		{
-			await context.TriggerTypingAsync(); //show in discord as typing
+   public class CommandsUngrouped : BaseCommandModule
+   {
+      //attributes
+      [Command("ping")]                              //define method
+      [Description("pongs when pinged.")]            //describe command
+      [Aliases("pong", "ping-pong")]                 //any other words for command
+      public async Task ping(CommandContext context) //no additional arguments are taken
+      {
+         await context.TriggerTypingAsync(); //show in discord as typing
 
-			var emoji = DiscordEmoji.FromName(context.Client, ":ping_pong:");	//creat emoji just cuz
-			await context.RespondAsync($"{emoji} pong! {context.Client.Ping}ms");	//respond
-		}
-//		[Command("ping")]		//overloads allowed in 4.x
-//		public async Task ping(CommandContext context, string copy)
-//		{
-//			await context.TriggerTypingAsync(); //show in discord as typing
-//
-//			var emoji = DiscordEmoji.FromName(context.Client, ":ping_pong:");     //creat emoji just cuz
-//			await context.RespondAsync($"{emoji} ponging back your string! {context.Client.Ping}ms"); //respond
-//			await context.RespondAsync(copy); //respond
-//		}
-		
-		[Command("roll")]
-		[Description("rolls a die of selected size. (Currently supports a single 6, or 20 sided die.")]
-		[Aliases("r")]
-		public async Task Random(CommandContext context, string die)
-		{
-			await context.TriggerTypingAsync(); //show in discord as typing
-			var random = new Random();
-			switch (die)
-			{
-				case "d6":
-				{
-					await context.RespondAsync($"🎲 You rolled a : {random.Next(1, 7)}");
-					break;
-				}
-				case "d20":
-				{
-					await context.RespondAsync($"🎲 You rolled a : {random.Next(1, 21)}");
-					break;
-				}
-				default:
-				{
-					await context.RespondAsync("you don't seem to have this kind of die, go buy one!");
-					break;
-				}
-			}
-		}
-	}
+         var emoji = DiscordEmoji.FromName(context.Client, ":ping_pong:");     //creat emoji just cuz
+         await context.RespondAsync($"{emoji} pong! {context.Client.Ping}ms"); //respond
+      }
+
+      [Command("ping")] //overloads allowed in 4.x
+      public async Task ping(CommandContext context, [RemainingText] [Description("string to be copied.")]
+                             string copy)
+      {
+         await context.TriggerTypingAsync(); //show in discord as typing
+
+         var emoji = DiscordEmoji.FromName(context.Client,
+                                           ":ping_pong:");                                         //creat emoji just cuz
+         await context.RespondAsync($"{emoji} ponging back your string! {context.Client.Ping}ms"); //respond
+         await context.RespondAsync(copy);                                                         //respond
+      }
+
+      [Command("roll")]
+      [Description("rolls a die of selected size. (Currently supports a single 6, or 20 sided die.")]
+      [Aliases("r")]
+      public async Task Random(CommandContext context, [Description("type of die to be rolled.")]
+                               string die)
+      {
+         await context.TriggerTypingAsync(); //show in discord as typing
+         var random = new Random();
+         switch (die)
+         {
+            case "d6":
+            {
+               await context.RespondAsync($"🎲 You rolled a : {random.Next(1, 7)}");
+               break;
+            }
+
+            case "d20":
+            {
+               await context.RespondAsync($"🎲 You rolled a : {random.Next(1, 21)}");
+               break;
+            }
+
+            default:
+            {
+               await context.RespondAsync("you don't seem to have this kind of die, go buy one!");
+               break;
+            }
+         }
+      }
+   }
+
+
+   [Group("admin")]
+   [Description("Administrative commands.")]
+   [Hidden]
+   [RequirePermissions(Permissions.ManageChannels)] //only members who can manage server have these perms
+   public class CommandsAdmin : BaseCommandModule
+   {
+      // all the commands will need to be executed as <prefix>admin <command> <arguments>
+
+      // this command will be only executable by the bot's owner
+      [Command("sudo")]
+      [Description("Executes a command as another user.")]
+      [Hidden]
+      [RequireOwner]
+      public async Task SudoAsync(CommandContext context, [Description("Member to execute as.")] DiscordMember member,
+                                  [RemainingText, Description("Command text to execute.")]
+                                  string commandString)
+      {
+         // note the [RemainingText] attribute on the argument.
+         // it will capture all the text passed to the command
+
+         // let's trigger a typing indicator to let
+         // users know we're working
+         await context.TriggerTypingAsync();
+
+         var command = context.CommandsNext.FindCommand(commandString, out var args);
+         if (command == null)
+            throw new CommandNotFoundException(commandString);
+
+         var fakeContext = context.CommandsNext.CreateFakeContext(member, context.Channel, commandString,
+                                                                  context.Prefix,
+                                                                  command, args);
+         await context.CommandsNext.ExecuteCommandAsync(fakeContext).ConfigureAwait(false);
+      }
+
+
+      [Command("nick")]
+      [Aliases("nickname")]
+      [Description("Changes a persons nickname.")]
+      [RequirePermissions(Permissions.ManageNicknames)]
+      public async Task NicknameAsync(CommandContext context,
+                                      [Description("Name of current members name")]
+                                      DiscordMember discordMember,
+                                      [RemainingText] [Description("New nickname for member.")]
+                                      string newNickname)
+      {
+         await context.TriggerTypingAsync();
+         try
+         {
+            // let's change the nickname, and tell the 
+            // audit logs who did it.
+            await discordMember.ModifyAsync(givenMember =>
+                                            {
+                                               givenMember.Nickname = newNickname;
+                                               givenMember.AuditLogReason =
+                                                  string.Concat("Edited by ", context.User.Username, "#",
+                                                                context.User.Discriminator, " (",
+                                                                context.User.Id, ")");
+                                            }).ConfigureAwait(false);
+
+            // let's make a simple response.
+            var emoji = DiscordEmoji.FromName(context.Client, ":+1:");
+
+            // and respond with it.
+            await context.RespondAsync(emoji);
+         }
+         catch (Exception exception)
+         {
+            // oh no, something failed, let the invoker now
+            var emoji = DiscordEmoji.FromName(context.Client, ":-1:");
+            await context.RespondAsync(emoji);
+         }
+      }
+   }
 }
